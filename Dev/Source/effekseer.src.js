9857b12110e25b5ef8619db7e41edf0ba3311268
tkx2020 <tkx2020@yahoo.co.jp>
2019-07-12 10:10:56 +0900

    replaced Pointer_stringify with UTF8ToString

--- Dev/Source/effekseer.src.js
+++ Dev/Source/effekseer.src.js
@@ -412,7 +412,7 @@ var effekseer = function () {
 		var memptr = Module._malloc(buffer.byteLength);
 		Module.HEAP8.set(new Uint8Array(buffer), memptr);
 		ptr = Core.GetglTFBodyURI(memptr, buffer.byteLength);
-		str = Module.Pointer_stringify(ptr);
+		str = Module.UTF8ToString(ptr);
 		Module._free(memptr);
 		return str;
 	};
