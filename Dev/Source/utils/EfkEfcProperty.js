b7e1a3de0c2fd9a48a757358ccb5488299ac48b1
durswd <swda.durl@gmail.com>
2019-10-16 01:30:37 +0900

    added a property loader of efkEfc

---
+++ Dev/Source/utils/EfkEfcProperty.js
@@ -0,0 +1,86 @@
+
+
+class BinaryReader {
+    constructor(buffer) {
+        this.buffer = buffer;
+        this.offset = 0;
+    }
+
+    getInt32() {
+        var ret = new DataView(this.buffer, this.offset, 4).getInt32(0, true);
+        this.offset += 4;
+        return ret;
+    }
+
+    getChunk4() {
+        var ret = new TextDecoder().decode(this.buffer.slice(this.offset, this.offset + 4));
+        this.offset += 4;
+        return ret;
+    }
+
+    getStringAsUTF16() {
+        var size = new DataView(this.buffer, this.offset, 4).getInt32(0, true);
+        this.offset += 4;
+
+        var str = new TextDecoder("utf-16le").decode(this.buffer.slice(this.offset, this.offset + (size * 2 - 2)));
+        this.offset += (size * 2);
+        return str;
+    }
+
+    isEoF() {
+        return this.offset >= this.buffer.byteLength;
+    }
+
+    skip(num) {
+        this.offset += num;
+    }
+}
+
+class EfkEfcProperty {
+    constructor() {
+        this.colorImages = []
+        this.normalImages = []
+        this.distortionImages = []
+        this.models = []
+        this.sounds = []
+    }
+}
+
+function loadEfkEfcInformation(buffer) {
+    function readStringArray(reader) {
+        var ret = []
+
+        var count = reader.getInt32();
+        for (var i = 0; i < count; i++) {
+            var str = reader.getStringAsUTF16();
+            ret.push(str)
+        }
+
+        return ret;
+    }
+
+    var reader = new BinaryReader(buffer);
+
+    var header = reader.getChunk4();
+    var version = reader.getInt32();
+
+    while (!reader.isEoF()) {
+        var chunk = reader.getChunk4();
+        var chunkSize = reader.getInt32();
+
+        if (chunk == "INFO") {
+            var info = new EfkEfcProperty();
+            info.colorImages = readStringArray(reader);
+            info.normalImages = readStringArray(reader);
+            info.distortionImages = readStringArray(reader);
+            info.models = readStringArray(reader);
+            info.sounds = readStringArray(reader);
+            return info;
+        }
+        else {
+            reader.skip(chunkSize);
+        }
+    }
+
+    return null;
+}
