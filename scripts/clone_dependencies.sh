f91673d68a6261f409d27f7ac46edfb89cbe18fc
durswd <swda.durl@gmail.com>
2020-05-12 22:37:37 +0900

    Change dependencies

--- scripts/clone_dependencies.sh
+++ scripts/clone_dependencies.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/effekseer/Effekseer --depth 1 ../Effekseer
+git clone -b 15x https://github.com/effekseer/Effekseer --depth 1 ../Effekseer
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
-./emsdk install sdk-1.39.14-64bit
-./emsdk activate sdk-1.39.14-64bit
+./emsdk install sdk-1.39.7-64bit
+./emsdk activate sdk-1.39.7-64bit
