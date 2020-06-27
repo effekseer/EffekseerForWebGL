be4769798365359605220d780c4afb61839e9611
durswd <swda.durl@gmail.com>
2020-06-27 21:06:41 +0900

    Update a dependency

--- scripts/clone_dependencies.sh
+++ scripts/clone_dependencies.sh
@@ -1,3 +1,4 @@
+git clone -b 15x https://github.com/effekseer/TestData.git
 git clone -b 15x https://github.com/effekseer/Effekseer --depth 1 ../Effekseer
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
