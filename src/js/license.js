7f736ddab6e3bf06620365d369c390d71f74cfbc
durswd <swda.durl@gmail.com>
2020-12-27 12:17:38 +0900

    Update Version

--- src/js/license.js
+++ src/js/license.js
@@ -1,5 +1,5 @@
 /*!
- *  Effekseer for WebGL v1.52n
+ *  Effekseer for WebGL v1.60Alpha5
  *  https://github.com/effekseer/EffekseerForWebGL
  *
  *  This software is licensed under the MIT License.
