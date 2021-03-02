3f8dd5ec0e446c54d2fb084360e9cfcb23539e1f
durswd <swda.durl@gmail.com>
2021-03-02 21:56:54 +0900

    fix

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -206,7 +206,7 @@ public:
 		{
 			if(backGroundTexture_ == 0)
 			{
-				void glGenTextures(1, &backGroundTexture_);
+				glGenTextures(1, &backGroundTexture_);
 			}
 			glBindTexture(GL_TEXTURE_2D, backGroundTexture_);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
