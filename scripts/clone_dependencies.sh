1f0db69852816845e890b859251a02ad43f18f83
swd <swda.durl@gmail.com>
2021-07-10 18:57:56 +0900

    Make Effekseer submodule (#55)
    Make Effekseer submodule
    # Conflicts:
    #	azure-pipelines.yml
    #	scripts/clone_dependencies.sh

--- scripts/clone_dependencies.sh
+++ scripts/clone_dependencies.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/effekseer/TestData.git
-git clone https://github.com/effekseer/Effekseer --depth 1 ../Effekseer
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 ./emsdk install sdk-1.39.14-64bit
