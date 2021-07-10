0ff4ad20968a2849a3d07243be414eb5a072e528
durswd <swda.durl@gmail.com>
2021-07-10 17:49:26 +0900

    fix

--- scripts/clone_dependencies.sh
+++ scripts/clone_dependencies.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/effekseer/TestData.git
-git clone -b 16x https://github.com/effekseer/Effekseer --depth 1 ../Effekseer
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 ./emsdk install sdk-1.39.14-64bit
