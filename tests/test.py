c335858b6e5240e115f4b71bb8987ce2fbd45ff3
durswd <swda.durl@gmail.com>
2022-06-04 18:13:07 +0900

    fix

--- tests/test.py
+++ tests/test.py
@@ -7,7 +7,7 @@ import base64
 import glob
 
 class CompareTest(unittest.TestCase):
-    def compare_results(self):
+    def test_compare_results(self):
         screenshot_file_paths = glob.glob('screenshots/*.png')
         for screenshot_file_path in screenshot_file_paths:
             file_name = os.path.basename(screenshot_file_path)
