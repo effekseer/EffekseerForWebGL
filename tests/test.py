b6521d0cdbfc2ea1e877735268a620e217897098
durswd <swda.durl@gmail.com>
2022-06-04 17:27:05 +0900

    fix

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,10 @@ class CompareTest(unittest.TestCase):
         screenshot_file_paths = glob.glob('screenshots', '*.png')
         for screenshot_file_path in screenshot_file_paths:
             file_name = os.path.basename(screenshot_file_path)
+            target_file_path = '../TestData/Tests/WebGL/' + file_name
+
+            print('compare : ' + screenshot_file_path + ' vs ' + target_file_path)
+            
             self.assertTrue(filecmp.cmp('../TestData/Tests/WebGL/' + file_name, screenshot_file_path), screenshot_file_path + ' is not equal')
 
 if __name__ == '__main__':
